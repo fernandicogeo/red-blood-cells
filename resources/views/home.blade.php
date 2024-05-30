1f882b49b05c706dce5f2b0d7b710027ffac10a4
fernandicogeo <fernandico.geovardo01@gmail.com>
2024-05-31 01:06:56 +0700

    fix: home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,10 @@
 @section('content')
 <div class="row">
     <div class="col-lg-12 d-flex justify-content-center">
-        <div id="card-red-blood" class="card" style="width: 50%;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+        <div id="card-red-blood" class="card" style="width: 50rem; border: none; background-color: black;">
+            <img src="/img/home.jpg" class="card-img-top" alt="Home">
+            <div class="card-body d-flex justify-content-center">
+              <a href="{{ route('login') }}" class="btn btn-danger px-5 py-2" style="background-color: var(--primary); border: none">Login</a>
             </div>
         </div>
     </div>
